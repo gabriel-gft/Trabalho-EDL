d72a94ee3b2153dcb8e4aad020ac5f115ce9a2a9
Mariana Guazzelli Costa <MarianaCosta3@edu.unisinos.br>
2026-05-21 20:59:17 -0300

    adiciona Fila e NoFila

--- src/br/com/atendimento/estruturas/Fila.java
+++ src/br/com/atendimento/estruturas/Fila.java
@@ -1,4 +1,64 @@
 package br.com.atendimento.estruturas;
 
 public class Fila {
+    private NoFila inicio;
+    private NoFila fim;
+    private int tamanho;
+
+    public Fila() {
+        this.inicio = null;
+        this.fim = null;
+        this.tamanho = 0;
+    }
+
+    public boolean estaVazia() {
+        return inicio == null;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void enfileirar(int numeroSenha, char tipoSenha) {
+        NoFila novo = new NoFila(numeroSenha, tipoSenha);
+
+        if (estaVazia()) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            fim.setProximo(novo);
+            fim = novo;
+        }
+
+        tamanho++;
+    }
+
+    public NoFila desenfileirar() {
+        if (estaVazia()) {
+            return null;
+        }
+
+        NoFila removido = inicio;
+        inicio = inicio.getProximo();
+
+        if (inicio == null) {
+            fim = null;
+        }
+
+        removido.setProximo(null);
+        tamanho--;
+
+        return removido;
+    }
+
+    public NoFila consultarPrimeiro() {
+        return inicio;
+    }
+
+    public NoFila consultarSegundo() {
+        if (inicio != null) {
+            return inicio.getProximo();
+        }
+        return null;
+    }
 }
--- src/br/com/atendimento/estruturas/NoFila.java
+++ src/br/com/atendimento/estruturas/NoFila.java
@@ -1,4 +1,34 @@
 package br.com.atendimento.estruturas;
 
 public class NoFila {
+    private int numeroSenha;
+    private char tipoSenha; // 'N' ou 'P'
+    private NoFila proximo;
+
+    public NoFila(int numeroSenha, char tipoSenha) {
+        this.numeroSenha = numeroSenha;
+        this.tipoSenha = tipoSenha;
+        this.proximo = null;
+    }
+
+    public int getNumeroSenha() {
+        return numeroSenha;
+    }
+
+    public char getTipoSenha() {
+        return tipoSenha;
+    }
+
+    public NoFila getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(NoFila proximo) {
+        this.proximo = proximo;
+    }
+
+    @Override
+    public String toString() {
+        return tipoSenha + String.valueOf(numeroSenha);
+    }
 }
