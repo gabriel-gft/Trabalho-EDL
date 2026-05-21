6bf5a547cf7fa866d14a8218e47c993d2705ebb6
gabriel-gft <gabriel.unisinos@gmail.com>
2026-05-21 20:54:50 -0300

    implementação das classes NoPilha e Pilha

--- src/br/com/atendimento/estruturas/NoPilha.java
+++ src/br/com/atendimento/estruturas/NoPilha.java
@@ -1,4 +1,16 @@
 package br.com.atendimento.estruturas;
 
-public class NoPilha {
+public class NoPilha<T> {
+    private NoPilha<T> proximo;
+    private T dado;
+
+    public NoPilha(T dado) {
+        this.dado = dado;
+    }
+
+    public NoPilha<T> getProximo() {
+        return proximo;
+    }
+    public void setProximo(NoPilha<T> proximo) { this.proximo = proximo; }
+    public T getDado() { return dado; }
 }
--- src/br/com/atendimento/estruturas/Pilha.java
+++ src/br/com/atendimento/estruturas/Pilha.java
@@ -1,4 +1,32 @@
 package br.com.atendimento.estruturas;
 
-public class Pilha {
+public class Pilha<T> {
+    private NoPilha<T> topo;
+    private int tamanho;
+
+    public Pilha() {
+        this.topo = null;
+        this.tamanho = 0;
+    }
+
+    public void empilhar (T dado) {
+        if (estaVazia()) {
+        topo = new NoPilha<>(dado);
+        } else {
+    NoPilha<T> novo_no = new NoPilha<>(dado);
+    novo_no.setProximo(topo);
+    topo = novo_no;
+        }
+        tamanho++;
+    }
+    public T desempilhar() {
+        if (estaVazia())
+            System.out.println("Pilha vazia");
+        tamanho--;
+        T dado_temp = topo.getDado();
+        topo = topo.getProximo();
+        return dado_temp;
+    }
+    public boolean estaVazia() { return tamanho == 0; }
+    public int getTamanho() { return tamanho; }
 }
