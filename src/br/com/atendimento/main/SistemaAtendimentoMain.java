ef97d49e6bb663a82834812df6ed2214ce82c867
gabriel-gft <gabriel.unisinos@gmail.com>
2026-05-21 21:27:05 -0300

    adicionar imports no SistemaAtendimentoMain e GerenciadorAtendimentos

--- src/br/com/atendimento/main/SistemaAtendimentoMain.java
+++ src/br/com/atendimento/main/SistemaAtendimentoMain.java
@@ -1,5 +1,5 @@
 package br.com.atendimento.main;
-
+import br.com.atendimento.servicos.GerenciadorAtendimentos;
 public class SistemaAtendimentoMain {
 
 }
--- src/br/com/atendimento/servicos/GerenciadorAtendimentos.java
+++ src/br/com/atendimento/servicos/GerenciadorAtendimentos.java
@@ -1,4 +1,10 @@
 package br.com.atendimento.servicos;
-
+import br.com.atendimento.estruturas.Fila;
+import br.com.atendimento.estruturas.NoFila;
+import br.com.atendimento.estruturas.Pilha;
+import br.com.atendimento.modelos.PostoAtendimento;
+import br.com.atendimento.modelos.Senha;
+import java.util.Random;
 public class GerenciadorAtendimentos {
+
 }
