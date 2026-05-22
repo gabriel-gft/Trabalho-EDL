0c26f8e789d46dae49c0c98e239e82239f6b953a
Eduardo <eduardooctavioviana@gmail.com>
2026-05-21 21:45:51 -0300

    classes PostoAtendimento e Senha

--- src/br/com/atendimento/modelos/PostoAtendimento.java
+++ src/br/com/atendimento/modelos/PostoAtendimento.java
@@ -1,4 +1,32 @@
 package br.com.atendimento.modelos;
 
 public class PostoAtendimento {
+    private final int id;
+    private boolean ativo;
+    private Senha senhaEmAtendimento;
+
+    public PostoAtendimento(int id, boolean ativo) {
+        this.id = id;
+        this.ativo = ativo;
+        this.senhaEmAtendimento = null;
+    }
+
+    public int getId() { return id; }
+    public boolean isAtivo() { return ativo; }
+    public void setAtivo(boolean ativo) { this.ativo = ativo; }
+    public boolean isLivre() { return ativo && senhaEmAtendimento == null; }
+    public Senha getSenhaEmAtendimento() { return senhaEmAtendimento; }
+
+    public void atender(Senha senha) { this.senhaEmAtendimento = senha; }
+    public void liberar() { this.senhaEmAtendimento = null; }
+
+    public String getStatus() {
+        if (!ativo) return "Inativo";
+        return senhaEmAtendimento == null ? "Livre" : "Ocupado";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Posto %d [%s]", id, getStatus());
+    }
 }
--- src/br/com/atendimento/modelos/Senha.java
+++ src/br/com/atendimento/modelos/Senha.java
@@ -1,4 +1,26 @@
 package br.com.atendimento.modelos;
 
 public class Senha {
+    private static int contadorNormal = 1;
+    private static int contadorPrioridade = 1;
+
+    private final int numero;
+    private final char tipo;
+
+    public Senha(char tipo) {
+        this.tipo = tipo;
+        this.numero = (tipo == 'N') ? contadorNormal++ : contadorPrioridade++;
+    }
+
+    public int getNumero() { return numero; }
+    public char getTipo() { return tipo; }
+    public String getIdentificador() { return tipo + String.valueOf(numero); }
+
+    public static void resetarContadores() {
+        contadorNormal = 1;
+        contadorPrioridade = 1;
+    }
+
+    @Override
+    public String toString() { return getIdentificador(); }
 }
